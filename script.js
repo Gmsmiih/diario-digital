a0b7fef7b1cf6d95507f8d53dd7ce2cbef390f36
Gmsmiih <miih1005gomess@gmail.com>
2026-06-01 15:46:09 -0300

    js alterações

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const button= document.getElementById("add-post")
+const button= document.getElementById("add-post");
+
+button.addEventListener("click", function() {
+    alert("Funcionou!");
+});
+
