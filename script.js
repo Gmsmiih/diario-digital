43e9268f2c373a7cab5a5d6ea5b2a60fea9260a1
Gmsmiih <miih1005gomess@gmail.com>
2026-06-15 16:13:44 -0300

    scipt.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 
 const button= document.getElementById("add-post");
 
+const formulario= document.getElementById("form");
+
 button.addEventListener("click", function() {
    formulario.classList.remove("hidden");
 });
 
 const feed= document.getElementById("feed");
 
-button.addEventListener("click", function() {
-    alert("Deu certo!");
-});
 
-const formulario= document.getElementById("form");
-
-formulario.classList.remove("hidden");
