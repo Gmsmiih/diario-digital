19144dfa0585a10c7673b5520d0cd389e8054abd
Gmsmiih <miih1005gomess@gmail.com>
2026-06-15 16:38:28 -0300

    const

--- script.js
+++ script.js
@@ -9,4 +9,8 @@ button.addEventListener("click", function() {
 
 const feed= document.getElementById("feed");
 
+const tit = document.getElementById("titulo");
 
+const content = document.getElementById("conteudo");
+
+const date = document.getElementById("data");
